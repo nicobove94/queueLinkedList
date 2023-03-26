b6cac8834f82affb7c306efda50ae07f867f3093
nicobove <nicolasbove7@gmail.com>
2023-03-26 14:12:23 -0400

    Added test directory and class

---
+++ Test/edu/psu/ist/NodeTest.java
@@ -0,0 +1,14 @@
+package edu.psu.ist;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeTest {
+    @org.junit.jupiter.api.Test
+    public void testConstructor() {
+        Node<String> stringNode = new Node<>("data");
+        assertEquals("data", stringNode.data);
+
+        Node<Integer> intNode = new Node<>(1);
+        assertEquals(1, intNode.data);
+    }
+}
