4ea7f5eac2a8d6f6a79e7d7f76e229c57e884e8f
nicobove <nicolasbove7@gmail.com>
2023-03-26 14:02:14 -0400

    Added to Node class

--- src/edu/psu/ist/Node.java
+++ src/edu/psu/ist/Node.java
@@ -1,5 +1,11 @@
 package edu.psu.ist;
 
-public class Node {
+public class Node<T> {
+    T data;
+    Node next;
 
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
 }
